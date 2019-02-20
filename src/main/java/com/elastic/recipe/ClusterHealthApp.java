b0b8d3ce178cfd4acbf8fe58e08166ce970fcf0e
dharada <dharada6@gmail.com>
2019-02-20 22:14:42 +0900

    first commit

---
+++ src/main/java/com/elastic/recipe/ClusterHealthApp.java
@@ -0,0 +1,40 @@
+package com.elastic.recipe;
+
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ClusterHealthApp {
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        TransportClient client = null;
+
+        try {
+            // create client for localhost es
+            client = new PreBuiltTransportClient(Settings.EMPTY)
+                    .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
+
+            ClusterHealthResponse clusterHealthResponse =
+                    client.admin().cluster().health(new ClusterHealthRequest()).actionGet();
+
+            System.out.println(clusterHealthResponse.getStatus().name());
+
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            try {
+                client.close();
+            } catch (Exception ex){
+                //ignore
+            }
+        }
+    }
+}
